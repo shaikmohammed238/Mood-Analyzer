84a6bb8e440145fad67a578b29ceaaaea47618f2
shaikmohammed238 <shaikmohammedghoue238@gmail.com>
2022-01-14 19:56:29 +0530

    added uc1 with test case

--- Moodanalyzer/UC1.cs
+++ Moodanalyzer/UC1.cs
@@ -3,7 +3,11 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace day20to22Moodanalyzer
-{
+{         /*Given a Message, ability
+          to analyse and respond
+           Happy or Sad Mood - Create MoodAnalyser Object - Call analyseMood function with message as
+           parameter and return Happy or Sad Mood
+           */
     public class UC1
     {
         private string message;
@@ -15,11 +19,11 @@ namespace day20to22Moodanalyzer
         {
           if (this.message.Contains("sad"))
             {
-                return "sad";
+                return "happy";
             }
             else
             {
-                return "happy";
+                return "sad";
             }
         }
     }
