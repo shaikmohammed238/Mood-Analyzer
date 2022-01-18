9f3e7a408d13b631fe752d5484f9bca0ad9fcd7e
shaikmohammed238 <shaikmohammedghoue238@gmail.com>
2022-01-18 10:50:40 +0530

    added uc4 with test case

---
+++ Moodanalyzer/uc4.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace day20to22Moodanalyzer
+{
+    /* UC1:-  Given a Message, ability to analyse and respond Happy or Sad Mood
+             - Create MoodAnalyser Object - Call analyseMood function with message as 
+             parameter and return Happy or Sad Mood.
+   */
+    /* UC2:- Given Null Mood Should Return Happy To make this Test Case pass Handle. 
+             - NULL Scenario using try catch and return Happy
+    */
+
+    /* UC3:- Inform user if entered Invalid Mood - In case of NULL or Empty Mood throw 
+             Custom Exception MoodAnalysisException. 
+             - Use Enum to differentiate the Mood Analysis Errors. 
+     */
+    public class uc4 
+    {
+        public string message;  //instance variable
+
+
+        public uc4 () //Constructors
+        {
+
+        }
+        public uc4 (string message) //parameterized constructor for intilizing instance member
+        {
+            this.message = message;
+            //this.methodName = methodName;
+        }
+        public string Analyzer()  //Analyzer method find mood
+        {
+            try  // Handling Exception
+            {
+                if (this.message.Equals(string.Empty))
+                {
+
+                    throw new uc4exception(uc4exception.ExceptionType.EMPTY_EXCEPTION, "Mood should not be empty");
+                }
+                else
+                {
+                    if (this.message.ToLower().Contains("happy"))
+                    {
+                        return "happy";
+                    }
+                    else
+                    {
+                        return "sad";
+                    }
+                }
+            }
+            catch (NullReferenceException ex)
+            {
+                //UC2 use -->// return ex.Message;
+                // return "happy";
+                throw new uc4exception(uc4exception.ExceptionType.NULL_EXCEPTION, "Mood should not be null");
+            }
+        }
+    }
+}
---
+++ Moodanalyzer/uc4factory.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace day20to22Moodanalyzer
+{
+    public class uc4factory
+    {/* UC4:- create default Constructor - Use Reflection to Create MoodAnalyser with default Constructor
+              - Create MoodAnalyserFactory and specify static method to create MoodAnalyser Objec
+              MoodAnalyserObject to create MoodAnalyser object 
+        */
+        public object CreateMoodAnalyzerObject(string className, string constructor)
+        {
+            //MoodAnalyzerProblem.MoodAnalyzer
+            string pattern = @"." + constructor + "$";
+            Match result = Regex.Match(className, pattern); //regex predefine class .pattern matching store result suppose pattern matching then create object than an constructor
+
+            if (result.Success) //success boolen property sucess hold true value other wise false //classma,e and patytern both are matching
+            {
+                try
+                {    //constructor and classname both are matching
+
+                    Assembly assembly = Assembly.GetExecutingAssembly();
+                    Type moodAnalyzerType = assembly.GetType(className);
+                    var res = Activator.CreateInstance(moodAnalyzerType); //Activator class
+                    return res;
+                }
+                catch (NullReferenceException)
+                {
+                    throw new uc4exception(uc4exception.ExceptionType.CLASS_NOT_FOUND, "class not found");//class name not maching that time we run
+                }
+            }
+            else
+            {
+                throw new uc4exception(uc4exception.ExceptionType.CONSTRUCTOR_NOT_FOUND, "constructor not found");//Constructor name not maching that time we run
+            }
+        }
+
+
+    }
+}
+
